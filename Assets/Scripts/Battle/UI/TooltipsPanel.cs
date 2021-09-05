44439ca014882e69705d2c452d5a6c84b77a1f63
Gabriel Pittol <gabrielpittol@hotmail.com>
2021-09-05 18:17:38 -0300

    Tooltips + Map Finished

--- Assets/Scripts/Battle/UI/TooltipsPanel.cs
+++ Assets/Scripts/Battle/UI/TooltipsPanel.cs
@@ -5,23 +5,62 @@ using UnityEngine.UI;
 public class TooltipsPanel : MonoBehaviour
 {
     public static TooltipsPanel Instance;
-    public Text tooltipsText;
- 
+    public Text tooltipsTextTitle;
+    public Text tooltipsTextDmg1;
+    public Text tooltipsTextDmg2;
+    public Text tooltipsTextDmg3;
+    public Text tooltipsTextEffectDefault;
+    public Text tooltipsTextEffect;
+
+    public Color tooltipColor = new Color32(236, 108, 30, 255);
+
     private void Awake()
     {
         Instance = this;
         ResetTooltipText();
+        //SetColor();
+    }
+
+    public void SetColor()
+    {
+        foreach(Transform t in GetComponentInChildren<Transform>())
+        {
+            t.gameObject.GetComponent<Text>().color = tooltipColor;
+        }
     }
 
     public void ResetTooltipText()
     {
-        print("Resetted");
-        tooltipsText.text = "";
-    }
+        tooltipsTextTitle.text = "";
+        tooltipsTextDmg1.text = "";
+        tooltipsTextDmg2.text = "";
+        tooltipsTextDmg3.text = "";
+        tooltipsTextEffectDefault.gameObject.SetActive(false);
+        tooltipsTextEffect.text = "";
+}
 
     public void ChangeTooltipText(RythmMove move)
     {
-        print("Changed");
-        tooltipsText.text = "\n" + move.name + "\nBase: " + move.baseDamage + "\nPerformance: " + move.performanceDamage + "\nExtra: " + move.extraDamage + "\nEffect: " + move.effect;
+        tooltipsTextTitle.text = move.name;
+        tooltipsTextDmg1.text = "Base: " + move.baseDamage;
+        tooltipsTextDmg2.text = "Performance: " + move.performanceDamage;
+        tooltipsTextDmg3.text = "Extra: " + move.extraDamage;
+        tooltipsTextEffectDefault.gameObject.SetActive(true);
+        tooltipsTextEffect.text = ConvertMoveEffectToString(move.effect);
+    }
+
+    public string ConvertMoveEffectToString(EMoveEffect effect)
+    {
+        switch(effect)
+        {
+            case EMoveEffect.PerformanceBased:
+                return "Performance Based";
+            case EMoveEffect.TempoDown:
+                return "Tempo Down";
+            case EMoveEffect.TempoUp:
+                return "Tempo Up";
+            default:
+                return effect.ToString();
+        }
     }
 }
--- Assets/Scripts/Rythm/RythmButton.cs
+++ Assets/Scripts/Rythm/RythmButton.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 public class RythmButton : MonoBehaviour
 {
     public RythmMove move;
+    public Color inactiveColor = new Color32(52, 160, 200, 255);
+    public Color activeColor = new Color32(236, 108, 30, 255);
 
     private void Start()
     {
@@ -17,15 +21,18 @@ public class RythmButton : MonoBehaviour
     public void OnMouseOver()
     {
         TooltipsPanel.Instance.ChangeTooltipText(move);
+        GetComponentInChildren<Text>().color = activeColor;
     }
 
     public void OnMouseExit()
     {
         TooltipsPanel.Instance.ResetTooltipText();
+        GetComponentInChildren<Text>().color = inactiveColor;
     }
 
     public IEnumerator ResizeBoxCollider()
     {
+        GetComponentInChildren<Text>().color = inactiveColor;
         yield return null;
         yield return null;
         GetComponent<BoxCollider2D>().size = new Vector2(GetComponent<RectTransform>().sizeDelta.x, GetComponent<RectTransform>().sizeDelta.y);
