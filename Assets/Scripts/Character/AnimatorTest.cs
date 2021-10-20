a61e54e18ec7ced6dec8764b8463153af72139cf
NathanSchneider <nathan.rainieri93@gmail.com>
2021-10-20 00:08:01 -0300

    Character Prefab

---
+++ Assets/Scripts/Character/AnimatorTest.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorTest : MonoBehaviour
+{
+    public static AnimatorTest instance;
+
+    public Animator animator = null;
+    public bool changeAnim = false;
+    
+    public static bool walkFw = false;
+    public static bool walkRd = false;
+    public static bool hip_Hop_0 = false;
+    public static bool hip_Hop_1 = false;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P) && changeAnim == false)
+        {
+            Debug.Log("here");
+            walkFw = true;
+        }
+        //else if (Input.GetKey(KeyCode.P) && changeAnim == true)
+        //{
+        //    changeAnim = false;
+        //}
+
+        PlayAnimations();
+    }
+
+    public void PlayAnimations()
+    {
+        if (hip_Hop_0)
+        {
+            animator.SetBool("Hip_Hop_0", true);           
+        }
+        else
+        {
+            animator.SetBool("Hip_Hop_0", false);
+        }
+
+    }
+
+    public void SetAnimation(bool var)
+    {
+        walkFw = var;
+    }
+}
