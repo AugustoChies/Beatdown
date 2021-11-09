86a023270c579f40094a233b08f4bdbca4a2e598
AugustoChies <guto_chies@hotmail.com>
2021-11-09 04:22:47 -0300

    battle rewards implemented + equipped icon

--- Assets/Scripts/Battle/UI/ConclusionBattleCanvas.cs
+++ Assets/Scripts/Battle/UI/ConclusionBattleCanvas.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 
 public class ConclusionBattleCanvas : UIElementScript
 {
@@ -13,14 +13,40 @@ public class ConclusionBattleCanvas : UIElementScript
     private GameObject defeatObject = null;
     [SerializeField]
     private string afterBattleScene = "";
-   
+
+    [SerializeField]
+    private TextMeshProUGUI _moneyReward = null;
+    [SerializeField]
+    private TextMeshProUGUI _moveReward = null;
+
 
     public override void Show()
     {
         base.Show();
-
+        
         victoryObject.SetActive(BattleController.Instance.battleWinnerPlayer);
         defeatObject.SetActive(!BattleController.Instance.battleWinnerPlayer);
+
+        if(BattleController.Instance.battleWinnerPlayer)
+        {
+            Inventory inventory = Inventory.Instance;
+            _moneyReward.text = BattleDataHolder.Instance.CurrentBattleData.RewardMoney.ToString();
+            inventory.Gold += BattleDataHolder.Instance.CurrentBattleData.RewardMoney;
+            if (BattleDataHolder.Instance.CurrentBattleData.RewardMove == null)
+            {
+                _moveReward.text = "Nothing";
+            }
+            else
+            {                
+                RythmMove newMove = BattleDataHolder.Instance.CurrentBattleData.RewardMove;
+                if (!inventory.PlayerData.ObtainedMoves.Contains(newMove))
+                {
+                    inventory.PlayerData.ObtainedMovesMovesID.Add(EquipmentManager.Instance.ListOfAllRythms.IndexOf(newMove));
+                    inventory.PlayerData.ObtainedMoves.Add(newMove);
+                }
+                _moveReward.text = newMove.moveName;
+            }
+        }
     }
 
     public void DoneButton()
