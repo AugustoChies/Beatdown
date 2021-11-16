1b0711b2a08477b71f75ec2e128001618c8ba605
Gabriel Pittol <gabrielpittol@hotmail.com>
2021-11-16 18:16:28 -0300

    Dialogue

--- Assets/Scripts/Ui/DialogueManager.cs
+++ Assets/Scripts/Ui/DialogueManager.cs
@@ -2,18 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
    public static DialogueManager Instance;
    [SerializeField] private GameObject dialoguePanel;
+   [SerializeField] private GameObject dialogueBoxRight;
+   [SerializeField] private GameObject dialogueBoxLeft;
+   [SerializeField] private TextMeshProUGUI dialogueText;
+
    [SerializeField] private Dialogue dialogueToPlay;
 
+   [SerializeField]private int currentIndex;
+
    private bool isPlayingDialogue;
+
+  
    private void Awake()
    {
       Instance = this;
       dialoguePanel.SetActive(false);
+      
+      //debug
+     // PlayDialogue(dialogueToPlay);
    }
 
    private void Update()
@@ -24,16 +36,43 @@ public class DialogueManager : MonoBehaviour
    public void PlayDialogue(Dialogue newDialogue)
    {
       dialogueToPlay = newDialogue;
-      //play dialogue here
+      dialogueBoxLeft.SetActive(false);
+      dialogueBoxRight.SetActive(false);
+      dialogueText.text = "";
+      isPlayingDialogue = true;
+
+      NextDialogueLine();
    }
 
    public void CheckForInputs()
    {
-      //check for button presses
+      if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Fire1"))
+      {
+         NextDialogueLine();
+      }
    }
 
    public void NextDialogueLine()
    {
-      //go to next dialogue line, if finished, end dialogue
+      if (currentIndex >= dialogueToPlay.dialogues.Length)
+      {
+         dialoguePanel.SetActive(false);
+         isPlayingDialogue = false;
+         return;
+      }
+
+      if (dialogueToPlay.dialogues[currentIndex].isRightSpeaker)
+      {
+         dialogueBoxRight.SetActive(true);
+         dialogueBoxLeft.SetActive(false);
+      }
+      else
+      {
+         dialogueBoxLeft.SetActive(true);
+         dialogueBoxRight.SetActive(false);
+      }
+
+      dialogueText.text = dialogueToPlay.dialogues[currentIndex].dialogueText;
+      currentIndex++;
    }
 }
