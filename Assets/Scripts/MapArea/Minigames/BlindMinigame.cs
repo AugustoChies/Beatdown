23aaaf286ed0e506d502adc2d5beca67dc3f0c6f
AugustoChies <guto_chies@hotmail.com>
2021-11-16 21:06:11 -0300

    progress blind game

---
+++ Assets/Scripts/MapArea/Minigames/BlindMinigame.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BlindMinigame : Minigame
+{
+    private List<RythmKey> Inputs = null;
+    private int currentInputIndex = 0;
+    private bool inputActive = false;
+    public int lowestInputAmount = 4;
+    public int highestInputAmount = 7;
+
+    public TextMeshProUGUI text = null;
+    public Image arrow = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Inputs = new List<RythmKey>();
+
+        for (int i = 0; i < lowestInputAmount - 1; i++)
+        {
+            Inputs.Add(GenerateInput());
+        }
+
+        StartCoroutine(NextRound(true));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            HandleRythmKey(RythmKey.Up);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            HandleRythmKey(RythmKey.Down);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            HandleRythmKey(RythmKey.Left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            HandleRythmKey(RythmKey.Right);
+        }
+    }
+
+    private void HandleRythmKey(RythmKey key)
+    {
+        if (!inputActive) return;
+
+        if(key == Inputs[currentInputIndex])
+        {
+            currentInputIndex++;
+            if(currentInputIndex >= Inputs.Count)
+            {
+                StartCoroutine(NextRound(true));
+            }
+        }
+        else
+        {
+            StartCoroutine(NextRound(false));
+        }
+    }
+
+    public RythmKey GenerateInput()
+    {
+        return (RythmKey)Random.Range(0, 4);
+    }
+
+    IEnumerator NextRound(bool win)
+    {
+        inputActive = false;
+        yield return new WaitForSeconds(1);
+
+        if(win)
+        {
+            if(Inputs.Count >= highestInputAmount)
+            {
+                performance = Inputs.Count / highestInputAmount;
+                ApplyGains();
+            }
+            else
+            {
+                Inputs.Add(GenerateInput());
+                ShowInputs();
+            }
+        }
+        else
+        {
+            performance = Inputs.Count / highestInputAmount;
+            ApplyGains();
+        }
+    }
+
+    public void ShowInputs()
+    {
+        text.text = "Watch!";
+        arrow.enabled = false;
+        StartCoroutine(ShowCoroutine());
+    }
+
+    IEnumerator ShowCoroutine()
+    {
+        for (int i = 0; i < Inputs.Count; i++)
+        {
+            yield return new WaitForSeconds(1);
+            arrow.enabled = true;
+            switch (Inputs[i])
+            {
+                case RythmKey.Down:
+                    arrow.rectTransform.localEulerAngles = new Vector3(0, 0, 0);
+                    break;
+                case RythmKey.Up:
+                    arrow.rectTransform.localEulerAngles = new Vector3(0, 0, 180);
+                    break;
+                case RythmKey.Left:
+                    arrow.rectTransform.localEulerAngles = new Vector3(0, 0, 270);
+                    break;
+                case RythmKey.Right:
+                    arrow.rectTransform.localEulerAngles = new Vector3(0, 0, 90);
+                    break;
+                default:
+                    break;
+            }
+        }
+        yield return new WaitForSeconds(1);
+        text.text = "Repeat!";
+        arrow.enabled = false;
+        inputActive = true;
+    }
+}
