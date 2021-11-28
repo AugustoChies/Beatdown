ae319d8e2f380008acb14766e2172fecb55e1787
Gabriel Pittol <gabrielpittol@hotmail.com>
2021-11-28 14:02:33 -0300

    Male/Female Hair Switcher

--- Assets/Scripts/Character/CosmeticController.cs
+++ Assets/Scripts/Character/CosmeticController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CosmeticController : MonoBehaviour
 {
     public List<GameObject> cosmeticsList = new List<GameObject>();
+    public GameObject maleHair;
+    public GameObject femaleHair;
+
     private void Start()
     {
         foreach (int i in Inventory.Instance.PlayerData.ListOfObtainedEquipmentsID)
@@ -12,5 +15,17 @@ public class CosmeticController : MonoBehaviour
             if (i >= cosmeticsList.Count) return;
             cosmeticsList[i].SetActive(true);
         }
+
+        if (Inventory.Instance.IsMale)
+        {
+            maleHair.SetActive(true);
+            femaleHair.SetActive(false);
+        }
+        else
+        {
+            maleHair.SetActive(false);
+            femaleHair.SetActive(true);
+        }
+
     }
 }
